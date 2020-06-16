5283ddad5256d92c9c1ddcbf0b7ad604141623a2
Oyebola Odukoya <oyebolaodukoya@MacBook-Air.local>
2020-06-16 09:46:19 -0400

    Final v1

--- Components/Camera.js
+++ Components/Camera.js
@@ -5,18 +5,18 @@ import { Header, Icon } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
-const CameraPage = ({ route, navigation, changePage  }) => {
+const CameraPage = ({ route, navigation, changePage }) => {
     const camera = useRef();
     const [hasCameraPermission, setHasPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
     const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
     const [flashIcon, setFlashIcon] = useState('ios-flash-off');
     const [hasRollPerm, setHasRollPerm] = useState(null);
-
+    const [showScreen, setShowScreen] = useState(false);
 
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
+            const { status } = await Permissions.askAsync(Permissions.CAMERA);
             setHasPermission(status === 'granted');
         })();
     }, []);
@@ -29,16 +29,21 @@ const CameraPage = ({ route, navigation, changePage  }) => {
     }, []);
 
     const pickImage = async () => {
-        try {
-            let result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                quality: 1,
-            });
-            if (!result.cancelled) {
-                usePicture(result)
+        if (hasRollPerm != true) {
+            setShowScreen(true)
+        } else {
+            setShowScreen(false)
+            try {
+                let result = await ImagePicker.launchImageLibraryAsync({
+                    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                    quality: 1,
+                });
+                if (!result.cancelled) {
+                    usePicture(result)
+                }
+            } catch (error) {
+                console.log(error);
             }
-        } catch (error) {
-            console.log(error);
         }
     };
 
@@ -51,13 +56,13 @@ const CameraPage = ({ route, navigation, changePage  }) => {
         }
     }
 
-    if (hasCameraPermission == null || hasCameraPermission == false || hasRollPerm == null || hasRollPerm == false) {
+    if (hasCameraPermission != true) {
         return (
-            <View style={{flex: 1, justifyContent:'center', alignItems:'center', width: 250, alignSelf:'center'}}>
-                <Image source={require('../assets/hoboscan.png')} style={{width: 300, height: 300}} />
-                <Text style={{fontSize:25}}>Ayyy!</Text>
-                <Text style={{textAlign:'center', fontSize:15}}>You are probably getting this screen because you don't have camera or cameral roll access enabled</Text>
-                <Button title="Enable Camera Access" onPress={()=> Linking.openSettings()}/>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: 250, alignSelf: 'center' }}>
+                <Image source={require('../assets/hoboscan.png')} style={{ width: 300, height: 300 }} />
+                <Text style={{ fontSize: 25 }}>Ayyy!</Text>
+                <Text style={{ textAlign: 'center', fontSize: 15 }}>You are probably getting this screen because you don't have Camera access enabled for HÖBOscan</Text>
+                <Button title="Enable Camera Access" onPress={() => Linking.openSettings()} />
             </View>
         );
     }
@@ -67,57 +72,69 @@ const CameraPage = ({ route, navigation, changePage  }) => {
     }
 
 
-    return (
-        <View style={{ flex: 1 }}>
-            <Camera ref={camera} style={{ flex: 1, justifyContent: 'space-between' }} type={type}>
-                <View style={styles.header}>
-                    <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-around' }}>
-                        <Icon
-                            onPress={() => {
-                                if (flash == Camera.Constants.FlashMode.off) {
-                                    setFlash(Camera.Constants.FlashMode.on);
-                                    setFlashIcon('ios-flash');
-                                } else {
-                                    setFlash(Camera.Constants.FlashMode.off);
-                                    setFlashIcon('ios-flash-off');
-                                }
-                            }}
-                            name={flashIcon} style={styles.button1} />
-                        <Icon
-                            onPress={() => {
-                                setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
-                            }}
-                            name='ios-reverse-camera' style={styles.button1} />
+    if (showScreen) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: 250, alignSelf: 'center' }}>
+                <Image source={require('../assets/hoboscan.png')} style={{ width: 300, height: 300 }} />
+                <Text style={{ fontSize: 25 }}>Ayyy!</Text>
+                <Text style={{ textAlign: 'center', fontSize: 15 }}>To select an image you need to have Camera Roll access enabled for HÖBOscan</Text>
+                <Button title="Enable Camera Roll Access" onPress={() => Linking.openSettings()} />
+                <Button title="Cancel" onPress={() => setShowScreen(false)} />
+            </View>
+        );
+    } else {
+        return (
+            <View style={{ flex: 1 }}>
+                <Camera ref={camera} style={{ flex: 1, justifyContent: 'space-between' }} type={type}>
+                    <View style={styles.header}>
+                        <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-around' }}>
+                            <Icon
+                                onPress={() => {
+                                    if (flash == Camera.Constants.FlashMode.off) {
+                                        setFlash(Camera.Constants.FlashMode.on);
+                                        setFlashIcon('ios-flash');
+                                    } else {
+                                        setFlash(Camera.Constants.FlashMode.off);
+                                        setFlashIcon('ios-flash-off');
+                                    }
+                                }}
+                                name={flashIcon} style={styles.button1} />
+                            <Icon
+                                onPress={() => {
+                                    setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
+                                }}
+                                name='ios-reverse-camera' style={styles.button1} />
+                        </View>
+
+                        <View style={{ flexDirection: 'row', flex: 2, justifyContent: 'center' }}>
+                            <Text style={{ color: 'white', fontSize: 20 }}>HÖBOscan</Text>
+                        </View>
+
+                        <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-around' }}>
+                            <Icon onPress={() => pickImage()} type='FontAwesome' name='photo' style={styles.button} />
+                            <Icon onPress={() => changePage(1)} type='Feather' name='edit' style={styles.button} />
+                        </View>
                     </View>
 
-                    <View style={{ flexDirection: 'row', flex: 2, justifyContent: 'center'}}>
-                        <Text style={{ color: 'white', fontSize: 20 }}>HÖBOscan</Text>
+
+                    <View style={{ position: "absolute", bottom: 30, alignSelf: 'center' }}>
+                        <Icon onPress={() => takePicture()}
+                            type='Entypo' name='circle' style={{ color: 'white', fontSize: 80, paddingBottom: 10 }} />
                     </View>
-
-                    <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-around' }}>
-                        <Icon onPress={() => pickImage()} type='FontAwesome' name='photo' style={styles.button} />
-                        <Icon onPress={() => changePage(1)} type='Feather' name='edit' style={styles.button} />
-                    </View>
-                </View>
-
-
-                <View style={{position: "absolute", bottom: 30, alignSelf:'center'}}>
-                    <Icon onPress={() => takePicture()}
-                        type='Entypo' name='circle' style={{ color: 'white', fontSize: 80, paddingBottom: 10 }}/>
-                </View>
-            </Camera>
-        </View>
-    );
+                </Camera>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     header: {
         position: 'absolute',
-        width:'100%',
-        flexDirection:'row',
-        paddingTop:45,
-        paddingLeft:10,
-        paddingRight:10,
+        width: '100%',
+        flexDirection: 'row',
+        paddingTop: 45,
+        paddingLeft: 10,
+        paddingRight: 10,
         backgroundColor: 'transparent',
         zIndex: 100
     },
@@ -127,17 +144,17 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 0 },
         shadowOpacity: 0.5,
-        shadowRadius: 5,  
+        shadowRadius: 5,
         elevation: 5
     },
 
     button: {
-        fontSize: 25, 
+        fontSize: 25,
         color: 'white',
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 0 },
         shadowOpacity: 0.5,
-        shadowRadius: 5,  
+        shadowRadius: 5,
         elevation: 5
     }
 });
